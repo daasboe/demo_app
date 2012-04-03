d2806225f2d71e5548e1848dd5fb6daefa83ce01
Daniel Aasboe <daniel.aasbo@gmail.com>
2012-04-03 20:38:31 +0200

    Finished demo app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
-  validates :content, :lenght => { :maximum => 140 }
+  validates :content, :length => { :maximum => 140 }
 end
